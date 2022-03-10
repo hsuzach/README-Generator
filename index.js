7537b3b9ca18fd1bd746cb5382fa4a2b8842ea28
hsuzach <hsuzach@gmail.com>
2022-03-10 14:17:03 -0800

    create prompt questions

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
+const inquirer = require('inquirer');
+const fs = require('fs')
+
+
 // array of questions for user
 const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Please provide the title of your ReadMe.',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide the description of your ReadMe.',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: "Please select your project's license type."
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+  },
+  
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Please enter your GitHub username.',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Please enter your email address.',
+  }
 
 ];
 
